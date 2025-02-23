1d2d178614df0fb8032808e3c6d4c8aed68c445a
new3bon <15679124+new3bon@users.noreply.github.com>
2025-02-23 16:14:00 +0900

    markdownライブラリよりmicromarkのほうが今風らしいので変更した

--- routes/upload.js
+++ routes/upload.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var markdown = require('markdown').markdown
+var micromark = require('micromark').micromark
+var gfm = require('micromark-extension-gfm').gfm
+var gfmHtml = require('micromark-extension-gfm').gfmHtml
 
 // POSTを受ける
 router.post('/', function(req, res, next) {
   fileContent = req.files.file.data.toString('utf8'); // ファイルの内容
-  res.render('upload', { content: markdown.toHTML(fileContent) });
+  res.render('upload', { content: micromark(fileContent,{
+    extensions: [gfm()],
+    htmlExtensions: [gfmHtml()]
+  }) });
 });
 
 module.exports = router;
